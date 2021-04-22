0345749903942e2d6a205454f9d75ceb7b6b2402
jordan.werre <jordan.r.werre@gmail.com>
2021-04-21 23:06:31 -0400

    basic slider complete

--- client/src/App.vue
+++ client/src/App.vue
@@ -18,17 +18,33 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url("https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@400;700&display=swap");
+
+*,
+*::after,
+*::before {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box; /*& force elements to inherit box-sizing property set in html &*/
+}
+
+:root {
+  --slider-sqr: 50rem;
+  --slider-sqr-small: 75vw;
+  --color-font-black: rgb(0, 0, 0);
+  --color-background-white: rgb(255, 255, 255);
+}
+
+html {
+  /* 62.5% of default font size (16px) = 10px = 1rem
+      Used to calculate rem values */
+  font-size: 62.5%;
+  /* box-sizing: border-box; */
 }
 
 .container {
-  max-width: 120rem;
+  width: 100%;
+  max-width: calc(var(--slider-sqr) + 10rem);
   margin: 8rem auto;
 }
 
@@ -36,5 +52,14 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 6rem auto;
 }
 </style>
--- client/src/components/Random.vue
+++ client/src/components/Random.vue
@@ -1,9 +1,36 @@
 <template>
   <div class="slider__container">
+    <div class="slider__dummy"></div>
     <swiper effect="flip" navigation :slides-per-view="3">
       <!-- virtual -->
-      <swiper-slide v-for="(talk, index) in talks" :key="index"
-        ><a :href="fullUrl(talk.url)" target="_blank">{{ talk.title }}</a>
+      <swiper-slide v-for="(talk, index) in talks" :key="index">
+        <div class="slider__content">
+          <a
+            :href="fullUrl(talk.url)"
+            target="_blank"
+            class="slider__content--link"
+          >
+            <div class="slider__content--title">
+              <!-- <a :href="fullUrl(talk.url)" target="_blank">{{ talk.title }}</a> -->
+              {{ talk.title }}
+            </div>
+            <div class="slider__content--speaker">
+              {{ getSpeaker(talk) }}
+            </div>
+            <div class="slider__content--conference">
+              {{ getConference(talk) }}
+            </div>
+            <div class="slider__content--topics">
+              <span
+                v-for="(topic, index) in talk.topics"
+                :key="talk.url + '_' + index"
+                class="slider__content--topic-item"
+                ><span v-if="index > 0"> &bull; </span
+                >{{ topic.topicname }}</span
+              >
+            </div>
+          </a>
+        </div>
         <!-- :virtualIndex="index" -->
       </swiper-slide>
     </swiper>
@@ -19,17 +46,17 @@
           {{ talk.title }}
         </div>
       </div>
-    </div> -->
+    </div>
 
-    <!-- <div class="random__slide-container">
+    <div class="random__slide-container">
       <div class="random__slide" v-for="talk in displayTalks" :key="talk._id">
         <span :class="{ red: currentTalk._id === talk._id }">
           {{ talk.title }}
         </span>
       </div>
-    </div> -->
+    </div>
 
-    <!-- <button class="random__btn" @click="lastTalk">Previous</button>
+     <button class="random__btn" @click="lastTalk">Previous</button>
     <button class="random__btn" @click="nextTalk">Next</button>
     <button class="random__btn" @click="shuffle">Shuffle Again</button>
     <template v-if="currentTalk">
@@ -80,15 +107,7 @@ export default {
       talks: [],
       baseURL: "",
       language: "",
-      // virtualData: {
-      //   talks: [],
-      // },
     };
-  },
-  computed: {
-    // slideStyles() {
-    //   return `left: ${this.virtualData.offset / 10}rem`;
-    // },
   },
   methods: {
     getTalks() {
@@ -107,46 +126,101 @@ export default {
     fullUrl(input) {
       return this.baseURL + input + this.language;
     },
+    getSpeaker(input) {
+      return input.speaker[0]?.name || "";
+    },
+    getConference(input) {
+      if (!input.conference[0]) return "";
+      const conf =
+        input.conference[0].month + " " + input.conference[0].year.toString();
+      return conf.trim();
+    },
   },
   created() {
     this.getTalks();
   },
-  // mounted() {
-  //   const self = this;
-  //   new Swiper(".swiper-container", {
-  //     virtual: {
-  //       talks: self.talks,
-  //       renderExternal(data) {
-  //         self.virtualData = data;
-  //       },
-  //     },
-  //   });
-  // },
 };
 </script>
 
 <style lang="scss" scoped>
 .slider {
-  max-width: 80vw;
-  background: white;
+  &__container {
+    display: inline-block;
+    position: relative;
+    width: 100%;
+    background: var(--color-background-white);
 
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    font-family: "Libre Baskerville", serif;
+  }
+
+  &__dummy {
+    margin-top: 100%;
+  }
+
+  &__content {
+    width: 100%;
+    height: 100%;
+    padding: 2.5rem;
+
+    &--link {
+      width: 100%;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      text-decoration: none;
+      color: var(--color-font-black);
+    }
+
+    &--title {
+      font-size: 2.4rem;
+    }
+
+    &--speaker {
+      font-size: 1.8rem;
+      padding-top: 1rem;
+    }
+
+    &--conference {
+      font-size: 1.6rem;
+      padding-top: 1rem;
+    }
+
+    &--topics {
+      padding-top: 2.2rem;
+    }
+
+    &--topic-item {
+      list-style: none;
+      text-transform: capitalize;
+      font-size: 1.4rem;
+    }
+  }
 }
 
 .swiper {
   &-container {
-    width: 30rem;
-    height: 30rem;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    width: 100%;
+    max-width: var(--slider-sqr);
+    height: 100%;
+    max-height: var(--slider-sqr);
     padding: 5rem;
   }
   &-slide {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 30rem;
-    height: 30rem;
+    //display: flex;
+    //justify-content: center;
+    //align-items: center;
+    width: 100%;
+    max-width: var(--slider-sqr);
+    height: 100%;
+    max-height: var(--slider-sqr);
+    box-shadow: 0.5rem 2rem 5rem rgba(0, 0, 0, 0.175);
 
     &-active {
       background: lightgray;
