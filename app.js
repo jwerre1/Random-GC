f44c3a67413b590ee8dd932663598ad05832f78f
jordan.werre <jordan.r.werre@gmail.com>
2021-05-17 08:33:58 -0400

    heroku deploy

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const cors = require("cors");
+//const cors = require("cors");
 const path = require("path");
 var serveStatic = require("serve-static");
 
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(serveStatic(path.join(__dirname, "./client/dist")));
-app.use(cors());
+//app.use(cors());
 
 const port = process.env.PORT || 3000;
 
